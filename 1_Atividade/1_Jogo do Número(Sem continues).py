45ddaa1bae7e508b10d2798d00e79d483436ec3a
devBandas <dudu.bandi@gmail.com>
2025-07-08 15:35:07 -0400

    Remoção do continue restante

--- 1_Atividade/1_Jogo do Número(Sem continues).py
+++ 1_Atividade/1_Jogo do Número(Sem continues).py
@@ -2,24 +2,24 @@ import random
 numero = (random.randint(1, 50))
 adivinho = 0
 while adivinho != numero :
-    adivinho_str = input("Tente adivinhar o número de 1 a 50! ")
-    if not adivinho_str.isdigit():
-        print("Coloque um número")
-        continue
-    adivinho = int(adivinho_str)   
-    if adivinho < numero :
-        if adivinho > 50 :
-             print("Coloque um número que seja no máximo 50.")
-        elif adivinho < 1 :
-             print("Coloque um número que seja no mínimo 1.")     
-        else :
-             print("Tente maior!")
-    elif adivinho > numero :
-        if adivinho > 50 :
-             print("Coloque um número que seja no máximo 50.")
-        elif adivinho < 1 :
-             print("Coloque um número que seja no mínimo 1.")
-        else :
-             print("Tente menor!")
+     adivinho_str = input("Tente adivinhar o número de 1 a 50! ")
+     if adivinho_str.isdigit():
+          adivinho = int(adivinho_str)   
+          if adivinho < numero :
+               if adivinho > 50 :
+                    print("Coloque um número que seja no máximo 50.")
+               elif adivinho < 1 :
+                    print("Coloque um número que seja no mínimo 1.")     
+               else :
+                    print("Tente maior!")
+          elif adivinho > numero :
+               if adivinho > 50 :
+                    print("Coloque um número que seja no máximo 50.")
+               elif adivinho < 1 :
+                    print("Coloque um número que seja no mínimo 1.")
+               else :
+                    print("Tente menor!")
+     else :
+         print("Coloque um número")               
 else :
         print("Você Acertou!!!")
